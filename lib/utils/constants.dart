daec2a8dde1db9b481cc52d3e83771cf12e221e2
tounsii007 <94169415+tounsii007@users.noreply.github.com>
2026-03-22 15:21:41 +0100

    Create constants.dart with app constants and configuration values

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static const String appName = 'Mobile Calculator';
+  static const String version = '1.0.0';
+  static const int defaultPrecision = 2;
+  static const String apiBaseUrl = 'https://api.example.com';
+  static const int timeoutDuration = 30; // seconds
+}
