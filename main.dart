c85e7ab918bfe9a5a06a103eb97de051cc06c452
tounsii007 <94169415+tounsii007@users.noreply.github.com>
2026-03-22 15:16:49 +0100

    Create main.dart file with Material app setup and theme configuration

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mobile Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Mobile Calculator'),
+        ),
+        body: Center(
+          child: Text('Welcome to Mobile Calculator!'),
+        ),
+      ),
+    );
+  }
+}
